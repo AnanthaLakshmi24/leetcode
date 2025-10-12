c05b11a2a988ec5ae60989c5ee82d0f1098c2621
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-10-12 12:37:41 +0530

    Time: 2 ms (53.77%), Space: 18 MB (21.92%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        freq={}
+        result = []
+        for num in nums:
+            freq[num] = freq.get(num,0)+1
+        for num in freq:
+            if freq[num]==2:
+                result.append(num)
+        return result
