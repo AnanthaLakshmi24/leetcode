1cfa6857a3aa1d18948f8b7cfec88b5efcee1cf5
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-10-14 21:11:54 +0530

    Time: 42 ms (28.04%), Space: 18.7 MB (9.58%) - LeetHub

--- 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.py
@@ -1,10 +1,13 @@
 import sys
 sys.set_int_max_str_digits(100000)
+# class Solution:
+    # def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+    #     ans=""
+    #     for n in num:
+    #         ans=ans+str(n)
+    #     l=int(ans)+k
+    #     return [int(d) for d in str(l)] 
 class Solution:
-    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        ans=""
-        for n in num:
-            ans=ans+str(n)
-        l=int(ans)+k
-        return [int(d) for d in str(l)] 
-        
+    def addToArrayForm(self, num: list[int], k: int) -> list[int]:
+        l = int("".join(map(str, num))) + k
+        return [int(d) for d in str(l)]
