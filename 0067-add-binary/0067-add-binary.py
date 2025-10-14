2c42f145f97ac33e3af42adf59ba8b57535d4b14
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-10-14 20:20:26 +0530

    Time: 0 ms (100%), Space: 17.8 MB (73.9%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,3 +1,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2)+int(b,2))[2:]
+        # return bin(int(a,2)+int(b,2))[2:]
+        x = int(a, 2)
+        y = int(b, 2)
+        while y:
+            x, y = x ^ y, (x & y) << 1
+        return bin(x)[2:]
