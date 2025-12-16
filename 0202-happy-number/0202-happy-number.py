c99fc3079e9e7bd36637239c4e942b7525a77883
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-12-16 20:47:16 +0530

    Time: 0 ms (100%), Space: 17.9 MB (17.54%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,15 +1,13 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        while True:
-            if n == 1 or n == 7:
-                return True
-            if n < 10:
-                return False
-
-            sum = 0
-            while n > 0:
-                temp = n % 10
-                sum += temp * temp
-                n = n // 10
-
-            n = sum
+        if(n==1 or n==7):
+            return True
+        elif(n<10):
+            return False
+        else:
+            sum =0
+            while(n>0):
+                temp = n%10
+                sum += temp*temp
+                n= n//10
+            return self.isHappy(sum)
